893fc38fc16da3f239f0176c768c511ac389fc3c
Lee Jin <leejin2018@kaist.ac.kr>
2024-05-14 20:58:49 +0900

    make tcp handler

---
+++ Communication/tcp_handler.h
@@ -0,0 +1,30 @@
+#ifndef TCPHANDLER_H
+#define TCPHANDLER_H
+
+#include <Ethernet.h>
+#include "dispatch.h"
+#include "BitStuffing.h"
+
+
+// TCPHandler singleton pattern 
+class TCPHandler{
+private:
+
+    TCPHandler() : TCPserver(TCPHandler::port) {}
+    TCPHandler(const TCPHandler&) = delete;
+    TCPHandler& operator=(const TCPHandler&) = delete;
+
+    EthernetServer TCPserver;
+    EthernetClient TCPclient; 
+    static uint16_t port;
+
+public:
+
+    static TCPHandler& getInstance();
+    void begin(byte mac[], IPAddress ip);
+    void clientHandle(); 
+    void messageHandle();
+    bool sendMessageToClient(const byte *message, size_t size);
+};
+
+#endif
--- Firmware_BoardCertification.ino
+++ Firmware_BoardCertification.ino
@@ -1,6 +1,24 @@
+#include <Ethernet.h>
+#include <SPI.h>
+#include <Dns.h>
+#include <Dhcp.h>
+
+#include "Communication/tcp_handler.h"
+
+#define SERVER_PORT 502
+#define sspin       53
+
+IPAddress ip(192, 168, 0, 141);
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+TCPHandler& tcpHandler = TCPHandler::getInstance();
+
 void setup()
 {
-	
+	Serial.begin(115200);
+    Ethernet.init(sspin);
+
+    tcpHandler.begin(mac, ip);
 }
 
 void loop()
